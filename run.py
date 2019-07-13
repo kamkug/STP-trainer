c827fe3914500001635074e026cd6445dff5a597
kamilucha123 <graczbat@o2.pl>
2019-07-13 14:50:25 +0100

    new.py ---> run.py

--- run.py
+++ run.py
@@ -10,8 +10,7 @@ try:
     stp_domain = utils.getInfile(sys.argv)
     stpDomain = STPTrainer(stp_domain)
     print(stpDomain.getSwitchPortRoles(stp_domain))
-    # Provide an output file
-    #utils.provideOutfile(stpDomain.stp_domain, "test123")
+    utils.provideOutfile(stpDomain.stp_domain, "test123")
 except  EOFError:
     print("\n[Ctrl-D] Shutting down...")
     exit(1)
